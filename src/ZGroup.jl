bc994bc7009400116ef6b3701e3439fdd2875ecd
Fabian Gans <fgans@bgc-jena.mpg.de>
2021-10-20 10:54:07 +0200

    Extend docs

--- src/ZGroup.jl
+++ src/ZGroup.jl
@@ -67,12 +67,14 @@ function Base.getindex(g::ZGroup, k)
 end
 
 """
-    zopen(s::AbstractStore, mode="r"; consolidated = false, path = "")
+    zopen(s::AbstractStore, mode="r"; consolidated = false, path = "", lru = 0)
 
 Opens a zarr Array or Group at Store `s`. If `consolidated` is set to "true",
 Zarr will search for a consolidated metadata field as created by the python zarr
 `consolidate_metadata` function. This can substantially speed up metadata parsing
-of large zarr groups.
+of large zarr groups. Setting `lru` to a value > 0 means that chunks that have been
+accessed before will be cached and consecutive reads will happen from the cache. 
+Here, `lru` denotes the number of chunks that remain in memory. 
 """
 function zopen(s::AbstractStore, mode="r"; consolidated = false, path = "", lru = 0)
     # add interfaces to Stores later    
