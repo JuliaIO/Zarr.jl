6e6bd14da15bad85c49f4163fa1dc9e237565a2e
Fabian Gans <fgans@bgc-jena.mpg.de>
2022-09-02 16:18:47 +0200

    Add and fix unit tests

--- src/metadata.jl
+++ src/metadata.jl
@@ -161,7 +161,7 @@ function Metadata(A::AbstractArray{T, N}, chunks::NTuple{N, Int};
         fill_value::Union{T, Nothing}=nothing,
         order::Char='C',
         filters::Nothing=nothing,
-        fill_as_missing = fill_value !== nothing && deprec_fillvalue(),
+        fill_as_missing = false,
     ) where {T, N, C}
     T2 = (fill_value === nothing || !fill_as_missing) ? T : Union{T,Missing}
     Metadata{T2, N, C, typeof(filters)}(
@@ -177,14 +177,6 @@ function Metadata(A::AbstractArray{T, N}, chunks::NTuple{N, Int};
 end
 
 Metadata(s::Union{AbstractString, IO},fill_as_missing) = Metadata(JSON.parse(s),fill_as_missing)
-
-function deprec_fillvalue()
-    @warn """In the future, fill values will not be interpreted as missing values by default. 
-    Please set the keyword argument `fill_as_missing` to a boolean accordingly. Setting to `true`
-    for now, but in the future `false` will be the default.
-    """
-    true
-end
 
 "Construct Metadata from Dict"
 function Metadata(d::AbstractDict, fill_as_missing)
