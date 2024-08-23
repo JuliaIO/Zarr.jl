ecdbeea5f9ebdd94f80048fc2eff7730dcaa8fa8
Anshul Singhvi <anshulsinghvi@gmail.com>
2024-08-23 15:03:53 -0700

    Semi working quantize filter

--- src/Filters/quantize.jl
+++ src/Filters/quantize.jl
@@ -13,20 +13,23 @@ struct QuantizeFilter{T, TENC} <: Filter{T, TENC}
     digits::Int32
 end
 
-function QuantizeFilter(; digits = 10, T = Float16, Tenc = DecodingType)
+function QuantizeFilter(; digits = 10, T = Float16, Tenc = T)
     return QuantizeFilter{T, Tenc}(digits)
 end
+
+QuantizeFilter{T, Tenc}(; digits = 10) where {T, Tenc} = QuantizeFilter{T, Tenc}(digits)
+QuantizeFilter{T}(; digits = 10) where T = QuantizeFilter{T, T}(digits)
 
 function zencode(data::AbstractArray, filter::QuantizeFilter{DecodingType, EncodingType}) where {DecodingType, EncodingType}
     arr = reinterpret(DecodingType, vec(data))
 
-    precision = 10^(-filter.digits)
+    precision = 10.0^(-filter.digits)
 
-    _exponent = log(precision, 10)
+    _exponent = log(10, precision) # log 10 in base `precision`
     exponent = _exponent < 0 ? floor(Int, _exponent) : ceil(Int, _exponent)
 
-    bits = ceil(log(10^(-exponent), 2))
-    scale = 2^bits
+    bits = ceil(log(2, 10.0^(-exponent)))
+    scale = 2.0^bits
 
     enc = @. round(scale * arr) / scale
 
