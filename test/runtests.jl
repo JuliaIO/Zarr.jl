dc2acc03cfd0670cee8aca541ba31d635e0f3892
Felix Cremer <fcremer@bgc-jena.mpg.de>
2025-03-06 10:21:56 +0100

    Add fill_value_decoding method for disambiguation

--- test/runtests.jl
+++ test/runtests.jl
@@ -147,6 +147,7 @@ end
         @test Zarr.fill_value_decoding("-", String) === "-"
         @test Zarr.fill_value_decoding("", Zarr.ASCIIChar) === nothing
         @test Zarr.fill_value_decoding("", Zarr.MaxLengthString{6,UInt8}) === Zarr.MaxLengthString{6,UInt8}("")
+        @test Zarr.fill_value_decoding(nothing, Zarr.ASCIICHar) === nothing
     end
 end
 
