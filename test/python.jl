266fb8000ea6af72dd2f1d46380839e4c62e0800
Nathan Zimmerberg <39104088+nhz2@users.noreply.github.com>
2025-08-22 17:52:42 -0400

    Update test/python.jl
    Co-authored-by: Mark Kittisopikul <mkitti@users.noreply.github.com>

--- test/python.jl
+++ test/python.jl
@@ -87,9 +87,8 @@ end
     g = zopen(pjulia)
     #Test group attributes
     @test g.attrs == groupattrs
-    for i=1:length(dtypes), co in compressors
+    for (t, co) in Iterators.product(dtypes, compressors)
         compstr,comp = co
-        t = dtypes[i]
         arname = string("a",t,compstr)
         ar = g[arname]
         @test ar.attrs == Dict("This is a nested attribute"=>Dict("a"=>5))
