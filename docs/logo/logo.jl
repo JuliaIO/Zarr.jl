b7481b3b069f2ef340c33b90a577bc1d88a4dec9
Lazaro Alonso <lazarus.alon@gmail.com>
2026-07-08 21:14:04 +0200

    mesh

--- docs/logo/logo.jl
+++ docs/logo/logo.jl
@@ -1,5 +1,6 @@
 using Colors
 using GLMakie
+using GeometryBasics
 using Random
 # using CairoMakie # don't use this one, the output comes with artifacts. GLMakie works fine.
 # CairoMakie.activate!()
@@ -38,10 +39,10 @@ function make_logo(; is_wireframe::Bool = true, transparency::Bool = false, seed
     tilt_angle = -pi / 8 # Tilt Z backwards by 22.5 degrees
     global_rot = Makie.qrotation(Vec3f(0,0,1), Float32(rot_angle)) * Makie.qrotation(Vec3f(1,0,0), Float32(tilt_angle))
 
-    transform_p(x_c, z_c) = begin
-        x_t = x_c
-        y_t = - (z_c - 4) * sin(tilt_angle)
-        z_t = (z_c - 4) * cos(tilt_angle)
+    transform_p3(x_l, y_l, z_l) = begin
+        x_t = x_l
+        y_t = y_l * cos(tilt_angle) - (z_l - 4) * sin(tilt_angle)
+        z_t = y_l * sin(tilt_angle) + (z_l - 4) * cos(tilt_angle)
         
         x_rot = x_t * cos(rot_angle) - y_t * sin(rot_angle)
         y_rot = x_t * sin(rot_angle) + y_t * cos(rot_angle)
@@ -49,37 +50,46 @@ function make_logo(; is_wireframe::Bool = true, transparency::Bool = false, seed
         return Point3f(x_rot + 4, y_rot + 4, z_t + 4)
     end
 
-    z_top_points = Point3f[]
-    z_bottom_points = Point3f[]
-    z_diag_points = Point3f[]
-    z_top_colors = Float32[]
-    z_bottom_colors = Float32[]
-    z_diag_colors = Float32[]
+    z_pts_2d = [
+        Point2f(-2.0, 6.4),        # 1
+        Point2f(2.0, 6.4),         # 2
+        Point2f(2.0, 5.434315),    # 3
+        Point2f(-1.034315, 2.4),   # 4
+        Point2f(2.0, 2.4),         # 5
+        Point2f(2.0, 1.6),         # 6
+        Point2f(-2.0, 1.6),        # 7
+        Point2f(-2.0, 2.565685),   # 8
+        Point2f(1.034315, 5.6),    # 9
+        Point2f(-2.0, 5.6)         # 10
+    ]
 
-    for x_c in -2:0.0025:2
-        push!(z_top_points, transform_p(x_c, 6))
-        push!(z_top_colors, 6f0)
-        
-        push!(z_bottom_points, transform_p(x_c, 2))
-        push!(z_bottom_colors, 2f0)
+    z_vertices = Point3f[]
+    z_colors = Float32[]
+    for p in z_pts_2d
+        push!(z_vertices, transform_p3(p[1], -0.399, p[2]))
+        push!(z_colors, p[2])
+    end
+    for p in z_pts_2d
+        push!(z_vertices, transform_p3(p[1], 0.399, p[2]))
+        push!(z_colors, p[2])
     end
 
-    z_diag_markersizes = Vec3f[]
-    for x_c in -2:0.0025:2
-        z_c = x_c + 4
-        
-        target_Z_top = z_c + 0.8 * sqrt(2) / 2
-        target_Z_bot = z_c - 0.8 * sqrt(2) / 2
-        
-        actual_Z_top = min(target_Z_top, 6.399)
-        actual_Z_bot = max(target_Z_bot, 1.601)
-        
-        actual_H = actual_Z_top - actual_Z_bot
-        actual_z_c = (actual_Z_top + actual_Z_bot) / 2
-        
-        push!(z_diag_points, transform_p(x_c, actual_z_c))
-        push!(z_diag_markersizes, Vec3f(0.0025, 0.8, actual_H))
-        push!(z_diag_colors, Float32(actual_z_c))
+    z_faces = GLTriangleFace[]
+    push!(z_faces, GLTriangleFace(1, 10, 9), GLTriangleFace(1, 9, 2), GLTriangleFace(2, 9, 3))
+    push!(z_faces, GLTriangleFace(9, 8, 3), GLTriangleFace(8, 4, 3), GLTriangleFace(8, 7, 4))
+    push!(z_faces, GLTriangleFace(4, 7, 6), GLTriangleFace(4, 6, 5))
+    push!(z_faces, GLTriangleFace(11, 19, 20), GLTriangleFace(11, 12, 19), GLTriangleFace(12, 13, 19))
+    push!(z_faces, GLTriangleFace(19, 13, 18), GLTriangleFace(18, 13, 14), GLTriangleFace(18, 14, 17))
+    push!(z_faces, GLTriangleFace(14, 16, 17), GLTriangleFace(14, 15, 16))
+    
+    boundary = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1]
+    side_start = 20
+    for i in 1:10
+        u, v = boundary[i], boundary[i+1]
+        push!(z_vertices, z_vertices[u], z_vertices[v], z_vertices[v+10], z_vertices[u+10])
+        push!(z_colors, z_colors[u], z_colors[v], z_colors[v+10], z_colors[u+10])
+        idx = side_start + (i-1)*4
+        push!(z_faces, GLTriangleFace(idx+1, idx+3, idx+2), GLTriangleFace(idx+1, idx+4, idx+3))
     end
 
     fig_out = with_theme(theme_light()) do
@@ -109,19 +119,43 @@ function make_logo(; is_wireframe::Bool = true, transparency::Bool = false, seed
         end
 
         if is_wireframe
-            left_marker = Rect3f(Vec3f(-0.025, -0.49, -0.49), Vec3f(0.05, 0.98, 0.98))
+            left_wf_marker = Rect3f(Vec3f(-0.025, -0.49, -0.49), Vec3f(0.05, 0.98, 0.98))
+            left_solid_marker = Rect3f(Vec3f(-0.1, -0.49, -0.49), Vec3f(0.2, 0.98, 0.98))
+            left_acc_pts = [p for (p, c) in zip(left_points, left_colors) if c in accent_colors]
+            left_acc_cls = [c for (p, c) in zip(left_points, left_colors) if c in accent_colors]
+            if !isempty(left_acc_pts)
+                meshscatter!(ax, left_acc_pts; marker=left_solid_marker, markersize=1f0, color=left_acc_cls, transparency=false, shading=true)
+            end
             for (p, c) in zip(left_points, left_colors)
-                wireframe!(ax, Rect3f(p .+ left_marker.origin, left_marker.widths); color=c, linewidth=1.5, transparency=transparency)
+                if !(c in accent_colors)
+                    wireframe!(ax, Rect3f(p .+ left_wf_marker.origin, left_wf_marker.widths); color=c, linewidth=1.5, transparency=transparency)
+                end
             end
 
-            back_marker = Rect3f(Vec3f(-0.49, -0.025, -0.49), Vec3f(0.98, 0.05, 0.98))
+            back_wf_marker = Rect3f(Vec3f(-0.49, -0.025, -0.49), Vec3f(0.98, 0.05, 0.98))
+            back_solid_marker = Rect3f(Vec3f(-0.49, -0.1, -0.49), Vec3f(0.98, 0.2, 0.98))
+            back_acc_pts = [p for (p, c) in zip(back_points, back_colors) if c in accent_colors]
+            back_acc_cls = [c for (p, c) in zip(back_points, back_colors) if c in accent_colors]
+            if !isempty(back_acc_pts)
+                meshscatter!(ax, back_acc_pts; marker=back_solid_marker, markersize=1f0, color=back_acc_cls, transparency=false, shading=true)
+            end
             for (p, c) in zip(back_points, back_colors)
-                wireframe!(ax, Rect3f(p .+ back_marker.origin, back_marker.widths); color=c, linewidth=1.5, transparency=transparency)
+                if !(c in accent_colors)
+                    wireframe!(ax, Rect3f(p .+ back_wf_marker.origin, back_wf_marker.widths); color=c, linewidth=1.5, transparency=transparency)
+                end
             end
 
-            bottom_marker = Rect3f(Vec3f(-0.49, -0.49, -0.025), Vec3f(0.98, 0.98, 0.05))
+            bot_wf_marker = Rect3f(Vec3f(-0.49, -0.49, -0.025), Vec3f(0.98, 0.98, 0.05))
+            bot_solid_marker = Rect3f(Vec3f(-0.49, -0.49, -0.1), Vec3f(0.98, 0.98, 0.2))
+            bot_acc_pts = [p for (p, c) in zip(bottom_points, bottom_colors) if c in accent_colors]
+            bot_acc_cls = [c for (p, c) in zip(bottom_points, bottom_colors) if c in accent_colors]
+            if !isempty(bot_acc_pts)
+                meshscatter!(ax, bot_acc_pts; marker=bot_solid_marker, markersize=1f0, color=bot_acc_cls, transparency=false, shading=true)
+            end
             for (p, c) in zip(bottom_points, bottom_colors)
-                wireframe!(ax, Rect3f(p .+ bottom_marker.origin, bottom_marker.widths); color=c, linewidth=1.5, transparency=transparency)
+                if !(c in accent_colors)
+                    wireframe!(ax, Rect3f(p .+ bot_wf_marker.origin, bot_wf_marker.widths); color=c, linewidth=1.5, transparency=transparency)
+                end
             end
         else
             meshscatter!(ax, left_points; marker=Rect3f(Vec3f(-0.1, -0.49, -0.49), Vec3f(0.2, 0.98, 0.98)), markersize=1f0, color=left_colors, transparency=transparency, shading=true)
@@ -130,15 +164,9 @@ function make_logo(; is_wireframe::Bool = true, transparency::Bool = false, seed
         end
 
         # Z shape
-        base_marker = Rect3f(Vec3f(-0.5, -0.5, -0.5), Vec3f(1.0, 1.0, 1.0))
         z_cmap = tuple.(reverse(colors[1:3]), 1)
-
-        meshscatter!(ax, z_top_points; marker=base_marker, markersize=Vec3f(0.0025, 0.798, 0.798),
-            rotation=global_rot, color=z_top_colors, colorrange=(2, 6), colormap=z_cmap, transparency=false)
-        meshscatter!(ax, z_bottom_points; marker=base_marker, markersize=Vec3f(0.0025, 0.798, 0.798),
-            rotation=global_rot, color=z_bottom_colors, colorrange=(2, 6), colormap=z_cmap, transparency=false)
-        meshscatter!(ax, z_diag_points; marker=base_marker, markersize=z_diag_markersizes,
-            rotation=global_rot, color=z_diag_colors, colorrange=(2, 6), colormap=z_cmap, transparency=false)
+        z_mesh = GeometryBasics.normal_mesh(z_vertices, z_faces)
+        mesh!(ax, z_mesh; color=z_colors, colorrange=(2, 6), colormap=z_cmap, transparency=false, shading=true, shininess=128f0, specular=Vec3f(1.0, 1.0, 1.0))
         
         return fig, ax
     end
@@ -147,7 +175,7 @@ end
 
 #wireframe
 fig, ax = make_logo(is_wireframe=true, transparency=true)
-# display(fig, update=false)
+display(fig, update=false)
 
 mkpath(joinpath(@__DIR__, "../src", "public"))
 save(joinpath(@__DIR__, "../src", "public", "logo.png"), alpha_colorbuffer(fig.scene))
